9b13e1f3b7cba89d62ad9e32ac6e8de5de21ee40
Harry Chen <harrychen0314@gmail.com>
2022-01-18 20:57:17 -0800

    update healthcheck

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -12,9 +12,4 @@ chmod 710 /judger/spj
 core=$(grep --count ^processor /proc/cpuinfo)
 n=$(($core*2))
 exec gunicorn --workers $n --threads $n --error-logfile /data/log/gunicorn.log --time 600 --bind 0.0.0.0:8080 server:app
-
-while true
-do
-    python3 /code/service.py
-    sleep 5
-done &
+exec bash /code/healthcheck.sh
---
+++ server/healthcheck.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+    python3 /code/service.py
+    sleep 5
+done &
